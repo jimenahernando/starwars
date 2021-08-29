044f516719d06faeaaa9af9b98c6a67b010f5dbf
jimena <jimenahernando@hotmail.com>
2021-08-29 22:25:55 +0200

    Mapeo rutas inicial y carga de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { InfoPersonComponent } from './components/info-person/info-person.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "/home" },
+  { path: "home", component: HomeComponent },
+  { path: "person/:idPerson", component: InfoPersonComponent },
+  { path: "**", redirectTo: "/home" }s
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
